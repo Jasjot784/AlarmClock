3a61f4e7c1d57556763c90b14411da9d5fb511de
Jasjot Singh <jasjot784@gmail.com>
2020-07-21 16:47:48 +0530

    All Intent Filters set

--- app/src/main/java/com/jasjotsingh/alarmclock/MainActivity.java
+++ app/src/main/java/com/jasjotsingh/alarmclock/MainActivity.java
@@ -11,23 +11,30 @@ import android.widget.Button;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private Button button,button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 createAlarm("Wake Up", Calendar.HOUR_OF_DAY,Calendar.MINUTE);
             }
         });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
     public void createAlarm(String message,int hour, int minutes) {
         Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM)
                 .putExtra(AlarmClock.EXTRA_MESSAGE, message)
-                //.putExtra(AlarmClock.EXTRA_DAYS,day)
                 .putExtra(AlarmClock.EXTRA_HOUR, hour)
                 .putExtra(AlarmClock.EXTRA_MINUTES, minutes);
         if (intent.resolveActivity(getPackageManager()) != null) {
